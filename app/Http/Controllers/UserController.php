73781400352e2f1f20fae913a8c0ff9c0473146d
alwanibrahim <alwanibrahim142@gmail.com>
2025-02-12 07:49:35 +0700

    commit category

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,12 +8,13 @@ use Illuminate\Http\JsonResponse;
 class UserController extends Controller
 {
     public function getUser(Request $request): JsonResponse
-    {   $token =$request->bearerToken();
+    {   
+        $token = $request->bearerToken();
         return response()->json([
             'status' => 'success',
-            'user' => $request->user(), // Mengambil data user yang sedang login
-            'token' => $token // **Tambahkan token di response**
-
+            'user' => $request->user(), // mengambil data user yang sedang login
+            'token' => $token  //sekalian tampilin token
         ]);
     }
 }
+
--- routes/api.php
+++ routes/api.php
@@ -3,13 +3,25 @@
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\CategoryController;
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::get('/user', [UserController::class, 'index']);
-Route::get ('/login', [AuthController::class, 'login']);
-Route::post ('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
-Route::middleware('auth:sanctum')->get('/user', [UserController::class, 'getUser']);
+//! auth
+Route::post('/register', [AuthController::class, 'register']); //* ini untuk register
+Route::post ('/login', [AuthController::class, 'login']); //*ini untuk login
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum'); //* ini untuk logout
+Route::middleware('auth:sanctum')->get('/user', [UserController::class, 'getUser']); //*ini untuk mengembil data user yg sedang login
 
 
+//!category
+Route::middleware('auth:sanctum')->prefix('categories')->group(function () {
+    Route::get('/', [CategoryController::class, 'index']);
+    Route::post('/', [CategoryController::class, 'store']);
+    Route::get('/{id}', [CategoryController::class, 'show']);
+    Route::put('/{id}', [CategoryController::class, 'update']);
+    Route::delete('/{id}', [CategoryController::class, 'destroy']);
+});
+
+
+// Route::get('/user', [UserController::class, 'index']);
+// Route::get ('/login', [AuthController::class, 'login']);
 // Route::middleware('auth:sanctum')->get('/user', [AuthController::class, 'getUser']);
